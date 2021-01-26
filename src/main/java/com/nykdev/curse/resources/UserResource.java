1eec97b8bc0cb5be6332330e4bfb4c3b43a15cb4
nyksonAlyson <nyksonalison@hotmail.com>
2021-01-26 10:35:13 -0300

    User insert

--- src/main/java/com/nykdev/curse/resources/UserResource.java
+++ src/main/java/com/nykdev/curse/resources/UserResource.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nykdev.curse.entities.User;
 import com.nykdev.curse.services.UserService;
@@ -34,6 +38,18 @@ public class UserResource {
 		 User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	@PostMapping
+	public ResponseEntity<User>insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		java.net.URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);	
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
